bad7398d09b2499e801e25f9f90ed2e10fc8e7b1
CrossDarkrix <crosseddarkrix@gmail.com>
2021-07-12 02:47:23 +0900

    Update SimplePortScanner.py

--- SimplePortScanner.py
+++ SimplePortScanner.py
@@ -2,20 +2,9 @@
 # -*- coding: utf-8 -*-
 
 from colorama import init as InitingColor, Fore
-import socket, sys
-from threading import Thread
+import socket, sys, concurrent.futures
 
 InitingColor()
-
-class ScanThread(Thread):
-    def __init__(self, iP, FaPort, LaPort):
-        Thread.__init__(self)
-        self.iP = iP
-        self.FaPort = FaPort
-        self.LaPort = LaPort
-
-    def run(self):
-        scan_port(self.iP, self.FaPort, self.LaPort)
 
 def Logo():
     print(Fore.RED + """___         _   ___               
@@ -23,33 +12,32 @@ def Logo():
  |  _/ _ \ '_|  _\__ \/ _/ _` | ' \ 
  |_| \___/_|  \__|___/\__\__,_|_||_|
 
- Simple TCP Port Scanner.
+ Simple Multithread TCP Port Scanner.
  Author: DarkRix.
  """ + Fore.RESET)
 
-def scan_port(iPAdress, FPort, LPort):
+def scan_port(iPAdress, PortNum):
     global Scan_Result
-    for PortNum in range(FPort, LPort):
-        try:
-            skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            skt.settimeout(0.02)
-            Scan_Result = skt.connect_ex((iPAdress, PortNum))
-            skt.close()
-            if Scan_Result == 0:
-                try:
-                    print(Fore.GREEN + '+ Open Port: {OPort}({ServiceName})'.format(OPort=PortNum, ServiceName=socket.getservbyport(PortNum)) + Fore.RESET)
-                except:
-                    print(Fore.GREEN + '+ Open Port: {OPort}({ServiceName})'.format(OPort=PortNum, ServiceName='unknow service') + Fore.RESET)
-            else:
-                 print(Fore.RED + '- Scan Port: {CPort}'.format(CPort=PortNum+1) + Fore.RESET, end='\r', flush=True)
-        except socket.error:
-            pass
-        except OverflowError:
-            pass
-        except KeyboardInterrupt:
-            sys.exit(0)
-        except:
-            pass
+    try:
+        skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        skt.settimeout(0.245)
+        Scan_Result = skt.connect_ex((iPAdress, PortNum))
+        skt.close()
+        if Scan_Result == 0:
+            try:
+                print(Fore.GREEN + '+ Open Port: {OPort}({ServiceName})'.format(OPort=PortNum, ServiceName=socket.getservbyport(PortNum)) + Fore.RESET)
+            except:
+                print(Fore.GREEN + '+ Open Port: {OPort}({ServiceName})'.format(OPort=PortNum, ServiceName='unknow service') + Fore.RESET)
+        else:
+            print(Fore.RED + '- Scan Port: {CPort}'.format(CPort=PortNum) + Fore.RESET, end='\r', flush=True)
+    except socket.error:
+        pass
+    except OverflowError:
+        pass
+    except KeyboardInterrupt:
+        sys.exit(0)
+    except:
+        pass
 
 def main():
     Logo()
@@ -71,16 +59,11 @@ def main():
         print('Error: InputPort Numbar')
         sys.exit(0)
     print(Fore.YELLOW + '\n\nScanning Port...\n' + Fore.RESET)
-    try:
-        StartScan = ScanThread(IPAdress, FastPort, LastPort)
-        StartScan.start()
-    except KeyboardInterrupt:
-        sys.exit(0)
-    except:
-        sys.exit(0)
-    StartScan.join()
+    StartScan = concurrent.futures.ThreadPoolExecutor(max_workers=None)
+    for Port in range(FastPort, LastPort):
+        StartScan.submit(scan_port, IPAdress, Port)
+    StartScan.shutdown()
     print(Fore.YELLOW + '\n\nScanning Done.' + Fore.RESET)
 
 if __name__== '__main__':
     main()
-
